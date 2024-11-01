b70399c981bddbdacd9deeda3980822ac546c0b4
Christian Kurz <chrikuvellberg@gmail.com>
2024-11-01 14:52:15 +0100

    comment out uninstallable libraries until xmake does BOM properly

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,6 @@
-add_requires("fmt", "boost", "nie-breakpad", "bzip2", "stack_alloc", "system_error2", "nontype_functional",
-  "concurrentqueue", "short_alloc", "skia-ref_ptr", "expected")
+add_requires("fmt", "boost", "nie-breakpad", "bzip2")
+-- add_requires("stack_alloc", "system_error2", "nontype_functional", "concurrentqueue", "short_alloc", "skia-ref_ptr",
+--  "expected")
 package("stack_alloc")
 do
   set_license("MIT")
