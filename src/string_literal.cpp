ba7907fbf3e3a81ee6bf1df9069ede6932c1a896
Christian Kurz <chrikuvellberg@gmail.com>
2024-11-13 23:35:28 +0100

    immediate string literals

--- src/string_literal.cpp
+++ src/string_literal.cpp
@@ -12,12 +12,20 @@ namespace nie {
   using namespace std::literals;
 
   namespace {
-    std::shared_mutex cache_mutex;
-    std::unordered_map<std::string_view, string_data const*> cache_{{"", nullptr}};
 #ifdef NIELIB_FULL
     nie::logger<"nie", "string_literal"> log;
 #endif
   } // namespace
+
+  struct cache_ptr_t {
+    std::shared_mutex cache_mutex;
+    std::unordered_map<std::string_view, string_data const*> cache_{{"", nullptr}};
+  }; // namespace
+
+  inline cache_ptr_t& cache_ptr() {
+    static cache_ptr_t x;
+    return x;
+  }
 
   struct dynamic_string_data : string_data {
     std::string t;
@@ -29,37 +37,35 @@ namespace nie {
 
   string::string(std::string_view text) {
     {
-      std::shared_lock lock(cache_mutex);
-      if (cache_.contains(text)) {
-        data_ = cache_.at(text);
+      std::shared_lock lock(cache_ptr().cache_mutex);
+      if (cache_ptr().cache_.contains(text)) {
+        data_ = cache_ptr().cache_.at(text);
         return;
       }
     }
-    std::unique_lock lock(cache_mutex);
-    if (cache_.contains(text)) {
-      data_ = cache_.at(text);
+    std::unique_lock lock(cache_ptr().cache_mutex);
+    if (cache_ptr().cache_.contains(text)) {
+      data_ = cache_ptr().cache_.at(text);
       return;
     }
     data_ = new dynamic_string_data(std::string(text));
 #ifdef NIELIB_FULL
     // log.trace<"register">("Registering (D) {:#X} as {}", std::bit_cast<std::size_t>(data_), data_->text());
 #endif
-    cache_.emplace(data_->text(), data_);
+    cache_ptr().cache_.emplace(data_->text(), data_);
   }
 
   [[gnu::visibility("default")]] void register_literal(string_data const* d) {
-    nie::register_startup([d] {
-      std::unique_lock lock(cache_mutex);
-      if (cache_.contains(d->text())) {
+    std::unique_lock lock(cache_ptr().cache_mutex);
+    if (cache_ptr().cache_.contains(d->text())) {
 #ifdef NIELIB_FULL
-        log.error<"register">("Duplicate {}", d->text());
+      log.error<"register">("Duplicate {}", d->text());
 #endif
-      }
-      nie::require(!cache_.contains(d->text()));
+    }
+    nie::require(!cache_ptr().cache_.contains(d->text()));
 #ifdef NIELIB_FULL
-      // log.trace<"register">("Registering (S) {:#X} as {}", std::bit_cast<std::size_t>(d), d->text());
+    // log.trace<"register">("Registering (S) {:#X} as {}", std::bit_cast<std::size_t>(d), d->text());
 #endif
-      cache_.emplace(d->text(), d);
-    });
+    cache_ptr().cache_.emplace(d->text(), d);
   }
 } // namespace nie
