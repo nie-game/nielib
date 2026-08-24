a7e70446f3cc3926632a406070b22d025a58943e
Christian Kurz <chrikuvellberg@gmail.com>
2026-08-24 13:17:47 +0200

    temporarily ignore Wunused-value

--- xmake.lua
+++ xmake.lua
@@ -53,7 +53,9 @@ function nielib_data()
     "-Werror=delete-non-virtual-dtor", "-Werror=return-type", "-Werror=switch",
     "-Werror=call-to-pure-virtual-from-ctor-dtor", "-Werror=defaulted-function-deleted",
     "-Werror=delete-non-virtual-dtor", "-Werror=abstract-final-class", "-ftemplate-backtrace-limit=0",
-    "-Werror=ignored-attributes", "-Werror=unused-value", "-Werror=uninitialized",
+    "-Werror=ignored-attributes", 
+    --"-Werror=unused-value",
+     "-Werror=uninitialized",
     "-Werror=tautological-constant-out-of-range-compare", "-Werror=argument-undefined-behaviour",
     "-Werror=vexing-parse", "-Werror=reorder-ctor", "-Werror=dangling", {public = true, force = true})
 
